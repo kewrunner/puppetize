767136d1e88c4c55995c0585b33c0d41ba613eda
Kew Runner <kewrunner@gmail.com>
2018-07-13 12:59:11 -0500

    updated password
    for security purposes

--- modules/users/manifests/root.pp
+++ modules/users/manifests/root.pp
@@ -1,7 +1,7 @@
 class users::root {
   user { 'root':
     ensure => present,
-    password => '$1$hwQfY6qi$9Bjy67R3yEXKaxpPIm.lM0',
+    password => 'hjkaljsdhflkjahsdlkfjhalksjd',
   } 
 
   file { "/root/.bashrc":
@@ -16,8 +16,8 @@ class users::root {
     user => "root",
     ensure => present,
     type => "ssh-rsa",
-    key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDCZuAEOu+poVvd3D1+d2MNpYZ+3td5vThowm+3p+z9P8UqWX8zoZZBdpHhuZ3o7+W/jDj/Hi+tmYQgG4VdxnP7JpGK1//Q04bPxQzB7P5z/H0P7DadB4bxCuU0YN6uv/RPON/YFfbU9yNwaxkCpZlChEkidhMqHf2Y2e3NIGZStOfKFCd9L9W3u+78eeqaJ7R5rdXt+pBxSJ6jceoa5tWQH6Rb7Iw1ORwiv6O9N0YEWu4Jh8ak5mpmH/d+CTlIpZcjX2fxk6oE0kcaXpHjaEZiiyQYl5MqSYQk3d+35WMkbwX39iF5NIgSbRTE9ZuWfUPzKyEakQIty2X6oPTFbwjf",
-    #name => "Damien"
+    key => "asdkjfhaskdfjlaksjdf;lkajs;dlfkjas;ldkjfkal;ksjdfl;",
+    #name => "peter"
   }
 }
                            
