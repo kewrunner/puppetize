7386a0906ad2f47fd17f6fbf1e50a6ee50e361f8
Kew Runner <kewrunner@gmail.com>
2018-07-13 12:58:09 -0500

    cleaned up tim
    for security purposes

--- modules/users/manifests/paul.pp
+++ modules/users/manifests/paul.pp
@@ -1,35 +1,35 @@
-class users::carvalhop-adm {
+class users::tim-adm {
 
-  user { 'carvalhop-adm':
-    comment => "Paul's account",
-    home => "/home/carvalhop-adm",
+  user { 'tim-adm':
+    comment => "tim's account",
+    home => "/home/tim-adm",
     shell => "/bin/bash",
     uid => 1500,
     gid => 1500,
     managehome => true,
     ensure => present,
-    password => '$6$Fvj6OGwd$Fbvkq6QZN7Ox8mg09EuYDwx5ILpdn4WGyW3zwuG4u87gbl7/MSX5wJOL6l6r0isZcek8e5HTNhC.tpQLyadqX/',
+    password => '#$%^U&^%FGYTFGUHIGYKYTGKJYFIYYGjygkuyfukygjkhgjk',
   }
 
-  group { 'carvalhop-adm':
+  group { 'tim-adm':
     gid => "1500",
     ensure => present
   }
   
-  file { "/home/carvalhop-adm/.bashrc":
+  file { "/home/tim-adm/.bashrc":
       ensure  => present,
-      owner   => carvalhop-adm,
-      group   => carvalhop-adm,
+      owner   => tim-adm,
+      group   => tim-adm,
       mode    => 0644,
       source  => "puppet:///modules/users/dot_bashrc_user"
   }
   
-  ssh_authorized_key{ 'carvalhop-adm':
-    user => "carvalhop-adm",
+  ssh_authorized_key{ 'tim-adm':
+    user => "tim-adm",
     ensure => present,
     type => "ssh-rsa",
-    key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA7+LgyEUX/EBIb4LrCPGyzO72wmVIMkhtQbumqR9DqxaaQTrlCzHEiUv+ZXf16qN44yE8Ml5Qg1Afecv/yNtjFcwqoEaVFFQ8CIL3F5ZUbj3O58z6F0Z8Ubs/ZsEewzaD13+PCcdgR9vI7DCYcWnoRnDHb6ZHkJxjMEwSijqVQz6oxiBAxi2o+hgeIvHi0wByJmrYY/cKPNJjNr7t9VL30H/lFSj8+ADAyb1AwN0Cz9dxkx7TmxCm0UI/6hIgf8WjQg8fDpK/8+Ug5IY/Dp5E04GZNsdFTKbdtITVo77xiWtWzLDcGzrqWCqSSy/yFpGcL9Oic0cwezcZ19NvcYzj8Q== carvalhop-adm@HSVPUPPET.quaero.com",
-    name => "Paul Carvalho"
+    key => "kj;dljaslkdfjlakjsdflakjsdlfkjalsdkfjal;skdjflka",
+    name => "tim"
   }
 }
                            
